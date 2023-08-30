0652cca65279187bc695574da60e1f1b843c20c4
Magnesium- <yyong007@outlook.com>
2023-08-30 21:56:27 +0800

    feat: upate version

--- remix.config.js
+++ remix.config.js
@@ -12,7 +12,8 @@ module.exports = {
     v2_errorBoundary: true,
     v2_routeConvention: true,
     v2_normalizeFormMethod: true,
-    unstable_dev: true
+    v2_dev: true,
+    v2_headers: true
   },
   async routes(defineRoutes) {
     const files = globSync('./app/routes/posts/*.mdx').map((f) => {
